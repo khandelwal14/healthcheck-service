c21d0f749430be7df3b5a10cb22aad9d0a1b2ef0
ankur2001 <33401101+ankur2001@users.noreply.github.com>
2019-11-25 21:34:38 -0800

    original

--- handlerbackup.js.js
+++
@@ -1,8 +0,0 @@
-// server.js// beforerequire('dotenv').config({ path: './variables.env' });
-var app = require('./src/app');var port = process.env.PORT || 3000;
-var server = app.listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
-// afterrequire('dotenv').config({ path: './variables.env' });
-var app = require('./src/app');var serverless = require('serverless-http');
-module.exports.handler = serverless(app);

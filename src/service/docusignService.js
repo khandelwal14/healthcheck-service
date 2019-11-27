e1d023f29f25577d12d84d27e82928c205032940
Murtuza Bohri <murtuzabohri@Murtuzas-MacBook-Pro.local>
2019-11-27 09:40:32 -0800

    Added docusign fucntion

---
+++ src/service/docusignService.js
@@ -0,0 +1,139 @@
+"use strict";
+
+const docusign = require('docusign-esign')
+    , path = require('path')
+    , fs = require('fs')
+    , process = require('process')
+    , basePath = 'https://demo.docusign.net/restapi'
+    , express = require('express')
+    , envir = process.env
+    ;
+    
+
+exports.handler = async function(event, context, callback) {
+    console.log("Payload Recieved11" +JSON.stringify(event));
+    let parsedEventObject = JSON.parse(JSON.stringify(event));
+    console.log("parsedEventObject . "+parsedEventObject.email);
+    let body = parsedEventObject.body ? JSON.parse(parsedEventObject.body) : parsedEventObject;
+    
+    let email = body.email;
+    console.log("email . "+email);
+    console.log("About to call SendEnvelope");
+    let sendEnvResp = await sendEnvelopeController(email);
+    let responseMessage = "Document  Successfully Sent for signature :"+JSON.stringify(sendEnvResp); 
+    const response = {
+        statusCode: 200,
+        headers: {
+            "x-custom-header": "My Header Value",
+        },
+        body: {message: " Envelope Sent Successfully"}
+    };
+    callback(null, response);
+};
+
+
+
+async function sendEnvelopeController (email) {
+    
+    try{
+  // Fill in these constants or use query parameters of ACCESS_TOKEN, ACCOUNT_ID, USER_FULLNAME, USER_EMAIL
+  // or environment variables.
+
+  // Obtain an OAuth token from https://developers.docusign.com/oauth-token-generator
+  const accessToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IjY4MTg1ZmYxLTRlNTEtNGNlOS1hZjFjLTY4OTgxMjIwMzMxNyJ9.eyJUb2tlblR5cGUiOjUsIklzc3VlSW5zdGFudCI6MTU3NDg3MTE5NiwiZXhwIjoxNTc0ODk5OTk2LCJVc2VySWQiOiJmOTE3YWEwMi1hY2VjLTQwZWQtYmE1Ny0wZGNmZjhkNDFmZTIiLCJzaXRlaWQiOjEsInNjcCI6WyJzaWduYXR1cmUiLCJjbGljay5tYW5hZ2UiLCJvcmdhbml6YXRpb25fcmVhZCIsImdyb3VwX3JlYWQiLCJwZXJtaXNzaW9uX3JlYWQiLCJ1c2VyX3JlYWQiLCJ1c2VyX3dyaXRlIiwiYWNjb3VudF9yZWFkIiwiZG9tYWluX3JlYWQiLCJpZGVudGl0eV9wcm92aWRlcl9yZWFkIiwiZHRyLnJvb21zLnJlYWQiLCJkdHIucm9vbXMud3JpdGUiLCJkdHIuZG9jdW1lbnRzLnJlYWQiLCJkdHIuZG9jdW1lbnRzLndyaXRlIiwiZHRyLnByb2ZpbGUucmVhZCIsImR0ci5wcm9maWxlLndyaXRlIiwiZHRyLmNvbXBhbnkucmVhZCIsImR0ci5jb21wYW55LndyaXRlIl0sImF1ZCI6ImYwZjI3ZjBlLTg1N2QtNGE3MS1hNGRhLTMyY2VjYWUzYTk3OCIsImF6cCI6ImYwZjI3ZjBlLTg1N2QtNGE3MS1hNGRhLTMyY2VjYWUzYTk3OCIsImlzcyI6Imh0dHBzOi8vYWNjb3VudC1kLmRvY3VzaWduLmNvbS8iLCJzdWIiOiJmOTE3YWEwMi1hY2VjLTQwZWQtYmE1Ny0wZGNmZjhkNDFmZTIiLCJhbXIiOlsiaW50ZXJhY3RpdmUiXSwiYXV0aF90aW1lIjoxNTc0ODcxMTk0LCJwd2lkIjoiNjJiM2QxNDgtOTM2My00MWQyLWI0MWUtOGQ5NjVmMjMzYmJiIn0.mLw69Kv9xOwdJ1r4HjCLaANLzzxRyUxK7Nk29pxBtpWEykz5aQ0Ws2_8Aaldr7onxdel8cmAQQZk7lQ3ABaiVsW7nRcq5hnOycQpFKrgjx7qilh7QvID2gdhlZL6wNizRvsGSDsHKiYnA7blAj6XJC3mPYde-RZuisFZa2KTC0M4uPowY7zuIGYX9Bq2t4_1Uq3yJ01BBeHoAjjqH6YuG7AtodWH1AoWOzrH8GL2AJJ0IBjFo1wXJi02IhEnYXn35MDBKA3gQfQDxKaW_2ZKbG77YIc7hpFB5jDWMtVvgdQCFqXrHtewsEc_SDlnjfx0JKaai8tcAA7YmpJO7B7fug";
+
+  // Obtain your accountId from demo.docusign.com -- the account id is shown in the drop down on the
+  // upper right corner of the screen by your picture or the default picture. 
+  const accountId = "9492754"; 
+
+  // Recipient Information:
+  const signerName = "Murtuz";
+  const signerEmail = email;
+
+  // The document you wish to send. Path is relative to the root directory of this repo.
+  const fileName = 'World_Wide_Corp_lorem.pdf';
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   *  The envelope is sent to the provided email address. 
+   *  One signHere tab is added.
+   *  The document path supplied is relative to the working directory 
+   */
+  const apiClient = new docusign.ApiClient();
+  apiClient.setBasePath(basePath);
+  apiClient.addDefaultHeader('Authorization', 'Bearer ' + accessToken);
+  // Set the DocuSign SDK components to use the apiClient object
+  docusign.Configuration.default.setDefaultApiClient(apiClient);
+
+  // Create the envelope request
+  // Start with the request object
+  const envDef = new docusign.EnvelopeDefinition();
+  //Set the Email Subject line and email message
+  envDef.emailSubject = 'Please sign this document sent from the Node example';
+  envDef.emailBlurb = 'Please sign this document sent from the Node example.'
+
+  // Read the file from the document and convert it to a Base64String
+  const pdfBytes = fs.readFileSync(path.resolve(__dirname, fileName))
+      , pdfBase64 = pdfBytes.toString('base64');
+  
+  // Create the document request object
+  const doc = docusign.Document.constructFromObject({documentBase64: pdfBase64,
+        fileExtension: 'pdf',  // You can send other types of documents too.
+        name: 'Sample document', documentId: '1'});
+
+  // Create a documents object array for the envelope definition and add the doc object
+  envDef.documents = [doc];
+
+  // Create the signer object with the previously provided name / email address
+  const signer = docusign.Signer.constructFromObject({name: signerName,
+        email: signerEmail, routingOrder: '1', recipientId: '1'});
+
+  // Create the signHere tab to be placed on the envelope
+  const signHere = docusign.SignHere.constructFromObject({documentId: '1',
+        pageNumber: '1', recipientId: '1', tabLabel: 'SignHereTab',
+        xPosition: '195', yPosition: '147'});
+
+  // Create the overall tabs object for the signer and add the signHere tabs array
+  // Note that tabs are relative to receipients/signers.
+  signer.tabs = docusign.Tabs.constructFromObject({signHereTabs: [signHere]});
+
+  // Add the recipients object to the envelope definition.
+  // It includes an array of the signer objects. 
+  envDef.recipients = docusign.Recipients.constructFromObject({signers: [signer]});
+  // Set the Envelope status. For drafts, use 'created' To send the envelope right away, use 'sent'
+  envDef.status = 'sent';
+
+  // Send the envelope
+  let envelopesApi = new docusign.EnvelopesApi()
+    , results
+    ;
+
+  try {
+    results = await envelopesApi.createEnvelope(accountId, {'envelopeDefinition': envDef})
+  } catch  (e) {
+    let body = e.response && e.response.body;
+    if (body) {
+      // DocuSign API exception
+      console.log (`<html lang="en"><body>
+                  <h3>API problem</h3><p>Status code ${e.response.status}</p>
+                  <p>Error message:</p><p><pre><code>${JSON.stringify(body, null, 4)}</code></pre></p>`);
+    } else {
+      // Not a DocuSign exception
+      throw e;
+    }
+  }
+  // Envelope has been created:
+  console.log("results "+JSON.stringify(results));
+  return results;
+  //if (results) {
+//    return "`<html lang="en"><body>
+  //              <h3>Envelope Created!</h3>
+    //            <p>Signer: ${signerName} &lt;${signerEmail}&gt;</p>
+      //          <p>Results</p><p><pre><code>${JSON.stringify(results, null, 4)}</code></pre></p>";
+  
+}catch(error){
+    console.log("Error In Send Envelpe catch "+error +JSON.stringify(error));
+}
+}
